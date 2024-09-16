a42c464bc617560663111fc0f6c7ae5f8bfa5f28
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-09-16 20:55:45 +0300

    Create DataStore

--- ContactListSwiftUI/Services/DataStore.swift
+++ ContactListSwiftUI/Services/DataStore.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+final class DataStore {
+    static let shared = DataStore()
+    
+    let names = ["Anna", "John", "Alex", "Kate",  "Fiby"]
+    let surnames = ["Blake",  "Smith", "Jones", "Miller", "Wilson"]
+    let phoneNumbers = ["8857710511", "8452208671", "8264492261", "8051253131", "8751234051"]
+    let emails = ["aaa@gmail.com", "kerk@mail.ru", "ssld@yandex.ru", "en18ee@gmail.com", "76rer@mail.ru"]
+    let id = [1, 2, 3, 4, 5]
+    
+    private init() {}
+}
