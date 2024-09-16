82dabd5874188d641e116f22f12481263f249fc5
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-09-16 17:04:50 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Анна Белова on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
